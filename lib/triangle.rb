36ce13bb020fdfa97fa52e71822af06fa0c60a79
Roxanne Gutierrez <roxannenguyenn@gmail.com>
2023-05-30 11:52:12 -0500

    commit tree 37ef48ba2b153e3e0b2eb1f263d38bb00a676c61

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,14 @@
 class Triangle
-  # write code here
+attr_accessor :sideA, :sideB, :sideC
+
+  def initialize (sideA, sideB, sideC)
+    sideA = sideA
+    sideB = sideB
+    sideC = sideC
+  end
+
+
+  def kind
+  
+  end
 end
